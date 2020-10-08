187a8679a57ebacf7ec6af16eee7029e9004f7e1
Shaheer2299 <72572695+Shaheer2299@users.noreply.github.com>
2020-10-08 23:06:12 +0500

    Web Project 1

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+$(".slider-one")
+.not(".slick-initialized")
+.slick({
+
+    autoplay:true,
+    autoplayspeed:300,
+    dots:true,
+
+    prevArrow:".site-slider .slider-btn.prev",
+    nextArrow:".site-slider .slider-btn.next",
+});
